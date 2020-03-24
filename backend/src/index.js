660f60acb0f7998c063f15cf75687c8a3c4982cf
Gerjunior <geraldojuni@hotmail.com>
2020-03-24 15:28:11 -0300

    Dia 2 - Criação do backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const routes = require('./routes')
+const cors = require('cors')
+
 
 const app = express()
 
-app.get('/', (request, response) => {
-    response.send(JSON.stringify('Hello, World!'))
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
     console.log("App listening on port 3333!")
