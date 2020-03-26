45b32d8a9cb8b22c987089bca06be288c60085f8
Gerjunior <geraldojuni@hotmail.com>
2020-03-26 10:09:32 -0300

    Dia 3 - Criação do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Routes from './routes'
+import './global.css'
 
-function App() {
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          OHIO SEKAI GOOD MORNING WORLD!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes />
     </div>
-  );
-}
-
-export default App;
+  )
+} 
