f6e9d8baec3106bc00d73565d94cdd89c168e054
Geraldo Jr. S <geraldojuni@hotmail.com>
2020-03-28 11:53:02 -0300

    Dia 5 - Funcionalidades avançadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
+const { errors } = require('celebrate')
 
 
 const app = express()
@@ -8,7 +9,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
-app.listen(3333, () => {
-    console.log("App listening on port 3333!")
-})
+module.exports = app
